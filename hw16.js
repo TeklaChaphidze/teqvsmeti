7a031b648be8132ec2662903b4e950a7ce2326d9
teklachapidze <teklachapidze599@gmail.com>
2024-11-19 15:57:35 +0400

    Add full project files

---
+++ hw16.js
@@ -0,0 +1,24 @@
+let addition = function add (a,b){
+    return a+b;
+}
+
+let l = function lgth(word) {
+    return word.length
+}
+
+function evenOrNot (a) {
+    if (a%2==0){
+        return "even number";
+    }
+    else {
+        return "odd number"
+    }
+}
+
+function firstElement (a){
+    return a[0];
+}
+
+function values (a){
+    return Object.values(a);
+}
